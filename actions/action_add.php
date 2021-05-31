dea94a5d2ef189c24f03592b8cf03fcf0172cd5e
Patrick <patrickpqdt87289Ggmail.com>
2021-05-31 13:22:05 -0300

    Password criptografia

--- actions/action_add.php
+++ actions/action_add.php
@@ -5,16 +5,24 @@
     $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
     $senha = filter_input(INPUT_POST, 'senha');
 
+
+
     if($nome && $email && $senha) {
         $sql = $db->prepare("SELECT * FROM register WHERE nome = :nome");
         $sql->bindValue(":nome", $nome);
         $sql->execute();
 
-        if ($sql->rowCount()=== 0) {
+        if ($sql->rowCount() === 0) {
+
+            $options = [
+                'cost' => 10,
+            ];
+            $senhaSegura = password_hash($senha, PASSWORD_DEFAULT,  $options);
+
             $sql = $db->prepare("INSERT INTO register (nome, email, senha) VALUES (:nome, :email, :senha)");
             $sql->bindValue(":nome", $nome);
             $sql->bindValue(":email", $email);
-            $sql->bindValue(":senha", $senha);
+            $sql->bindValue(":senha", $senhaSegura);
             $sql->execute();
 
             header("Location: ../index.php");
